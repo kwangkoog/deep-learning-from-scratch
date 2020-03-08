cb6429251b6f2fc9502a180280c5c1d9234b2438
Kwang-koog Lee <61933692+kwangkoog@users.noreply.github.com>
2020-03-08 23:47:14 +0900

    Update train_neuralnet.py

--- ch04/train_neuralnet.py
+++ ch04/train_neuralnet.py
@@ -23,9 +23,14 @@ train_acc_list = []
 test_acc_list = []
 
 # 1에폭당 반복 수
+# 60000개 데이터에서 100개의 미니배치를 나누면 600개가 한 에폭 당 반복 수가 됨
 iter_per_epoch = max(train_size / batch_size, 1)
 
-# 10000번에 대해 경사 하강법을 수행
+# 훈련 횟수인 10000번 동안 경사 하강법을 수행함
+# 기울기 계산이 들어갈 때 batch 갯수를 가져가서 손실함수를 구하고,
+# 그 다음 경사 하강법에 의해 현재 기울기를 구하고,
+# 다음 스텝에 필요한 weight, bias를 갱신한다
+
 for i in range(iters_num):
 
     # 매 반복마다 미니배치 획득
@@ -50,6 +55,9 @@ for i in range(iters_num):
     train_loss_list.append(loss)
     
     # 1에폭당 정확도 계산
+    # 신경망 학습의 목표는 범용적인 능력이므로 학습 도중 정기적으로 훈련 데이터와 시험 데이터를 대상으로 정확도를 기록함
+    # 1에폭은 학습에서 훈련 데이터를 모두 소진했을 때의 횟수에 해당
+    # 현재 예에서는 600개가 하나의 에폭이므로 10000번 수행을 600으로 나누면 17번이 나옴 (17번마다 기록을 수행)
     if i % iter_per_epoch == 0:
         # 훈련데이터에 대한 정확도 산출
         train_acc = network.accuracy(x_train, t_train)
@@ -58,7 +66,7 @@ for i in range(iters_num):
         train_acc_list.append(train_acc)
         test_acc_list.append(test_acc)
         print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
-
+        
 # 그래프 그리기
 markers = {'train': 'o', 'test': 's'}
 x = np.arange(len(train_acc_list))

31a885c305017b20daa2cb9c455bcd5f8c1308c2
Kwang-koog Lee <61933692+kwangkoog@users.noreply.github.com>
2020-03-10 03:30:33 +0900

    Update layer_naive.py

--- ch05/layer_naive.py
+++ ch05/layer_naive.py
@@ -1,11 +1,12 @@
 # coding: utf-8
 
-
+# 입력 x와 y에 대한 forward / backward 연산을 수행하는 곱하기 노드 설계
 class MulLayer:
     def __init__(self):
         self.x = None
         self.y = None
 
+    # 입력 x와 y에 대한 곱    
     def forward(self, x, y):
         self.x = x
         self.y = y                
@@ -13,13 +14,16 @@ class MulLayer:
 
         return out
 
+    # 입력으로 dout이 들어오면 
+    # dx는 dout에 y를 곱
+    # dy는 dout에 x를 곱
     def backward(self, dout):
         dx = dout * self.y  # x와 y를 바꾼다.
         dy = dout * self.x
 
         return dx, dy
 
-
+# 입력 x와 y에 대한 덧셈
 class AddLayer:
     def __init__(self):
         pass
